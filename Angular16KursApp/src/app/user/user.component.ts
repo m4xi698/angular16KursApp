5259b096680ad0d2d24a43e9e496fee923909618
Maximilian.Dammert <Maximilian.Dammert@cas.de>
2024-11-19 13:59:49 +0100

    First basic steps for building an application with angular

---
+++ Angular16KursApp/src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required : true}) id! : string;
+  @Input({required : true}) avatar! : string;
+  @Input({required : true}) name! : string;
+  @Output() selectUserEvent = new EventEmitter();
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
+  onUserClick(){
+    this.selectUserEvent.emit(this.id)
+  }
+}
